a329a09d72718841a7c390cdec1b03b4b1292aeb
Andrew Eskenazi <drewesk@gmail.com>
2017-06-26 11:40:20 -0600

    Adding user auth files!

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-app.get("/", function(req, res, next) {
-    res.json({
-      connected: true
-    })
+var index = require('./routes/index');
+var user = require('./routes/user');
+var auth = require('./auth'); //shorthand for ./auth/index.js
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/auth', auth);
+app.use('/', index);
+app.use('/user', user);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log(`Listening on ${port}`);
+// error handler
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: `${err.message} ☔️`,
+    error: res.locals.error = req.app.get('env') === 'development' ? err : {}
+  })
 });
+
+module.exports = app;
