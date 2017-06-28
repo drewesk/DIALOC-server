798ccc02080afb10b3f225dd8309562488498515
Andrew Eskenazi <drewesk@gmail.com>
2017-06-27 22:53:58 -0600

    Added a favorite column to the antipode table.

--- migrations/03_antipode_table.js
+++ migrations/03_antipode_table.js
@@ -3,11 +3,12 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("antipode_table", function(table) {
     table.increments("id").primary();
     table.integer("location_a_id").unsigned();
-    table.foreign("location_a_id").references("location_table.id")
+    table.foreign("location_a_id").references("location_table.id");
     table.integer("location_b_id").unsigned();
-    table.foreign("location_b_id").references("location_table.id")
+    table.foreign("location_b_id").references("location_table.id");
+    table.boolean('favorite').notNullable().defaultTo(false);
     table.integer("user_id").unsigned();
-    table.foreign("user_id").references("user.id")
+    table.foreign("user_id").references("user.id");
   });
 };
 
