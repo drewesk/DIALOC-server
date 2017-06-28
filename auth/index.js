7e314c0a47b7e615defbdcb78cdbb49c15153219
Andrew Eskenazi <drewesk@gmail.com>
2017-06-28 14:06:09 -0600

    Updating master.

--- auth/index.js
+++ auth/index.js
@@ -19,6 +19,7 @@ router.post('/signup', (req, res, next) => {
           bcrypt.genSalt(8, function(err, salt) {
             bcrypt.hash(req.body.password, salt, function(err, hash) {
               const user = {
+                name: req.body.name,
                 email: req.body.email,
                 password: hash,
                 created_at: new Date()
@@ -27,8 +28,7 @@ router.post('/signup', (req, res, next) => {
               User.create(user)
                 .then((id) => {
                   res.json({
-                    hash,
-                    id,
+                    user,
                     message: 'User has been created 🍜'
                   });
                 });
@@ -62,10 +62,8 @@ router.post('/login', (req, res, next) => {
                 });
 
                 res.json({
-                  id: user.id,
-                  message: 'Logged in 👍'
-                  name: user.name
-                  email: user.email
+                    user,
+                    message: "logged in 👍 🤡!!!!!!!!"
                 });
               } else {
                 next(new Error('Invalid Login'));
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const queries = require("../db/queries");
+const userQueries = require("../db/user");
 
 
 router.get("/", function(req, res, next) {
   queries.getAll().then(function(users) {
     res.json(users);
+  });
+});
+
+router.get("/:id", function(req, res, next) {
+  userQueries.getOneByID(req.params.id).then(function(user) {
+    res.json(user);
+  });
+});
+
+router.get("/:email/email", function(req, res, next) {
+  userQueries.getOneByEmail(req.params.email).then(function(user) {
+    res.json(user);
   });
 });
 
